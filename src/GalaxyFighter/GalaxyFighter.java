30f8b2d2fcdd051895d9032140847bfde2691015
udany <long.eagle.goodies@gmail.com>
2018-05-30 00:17:19 -0300

    Larger window

--- src/GalaxyFighter/GalaxyFighter.java
+++ src/GalaxyFighter/GalaxyFighter.java
@@ -9,17 +9,17 @@ import java.awt.*;
 
 public class GalaxyFighter extends Game {
     public GalaxyFighter() {
-        super(640, 480);
+        super(1280, 720);
         setTitle("Galaxy Fighter");
 
         BaseShip ship;
 
         ship = new Bombardier(this);
-        ship.setPosition(240, 480-48);
+        ship.setPosition(580, 480-48);
         objectList.add(ship);
 
         ship = new EdgeLiner(this);
-        ship.setPosition(380, 480-48);
+        ship.setPosition(680, 480-48);
         objectList.add(ship);
 
         ship = new BaseEnemyShip(this);
