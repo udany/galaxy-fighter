0e85be0f90848bd81e51c19d2f6c5a56d332e8de
udany <long.eagle.goodies@gmail.com>
2018-06-02 12:02:14 -0300

    Explosion sprite

--- src/GalaxyFighter/objects/bullets/SimpleBullet.java
+++ src/GalaxyFighter/objects/bullets/SimpleBullet.java
@@ -13,6 +13,10 @@ public class SimpleBullet extends BaseBullet {
 
         currentSprite = new Sprite(16, 16, "/images/Bullets.png");
         currentSprite.origin.set(5, 5);
+
+        explodeSprite = new Sprite(16, 16, "/images/Explosion_small.png");
+        explodeSprite.origin.set(5, 5);
+        explodeSprite.setFramesPerFrame(6);
     }
 
     public void setType(int type) {
