fc5549e871efe8c780422cdf15c087c1b8c9a08d
udany <long.eagle.goodies@gmail.com>
2018-06-02 12:02:44 -0300

    Enemies explode

--- src/GalaxyFighter/objects/ship/Enemy/BaseEnemyShip.java
+++ src/GalaxyFighter/objects/ship/Enemy/BaseEnemyShip.java
@@ -1,11 +1,15 @@
 package GalaxyFighter.objects.ship.Enemy;
 
+import GalaxyFighter.objects.bullets.BaseBullet;
 import GalaxyFighter.objects.ship.BaseShip;
 import GalaxyFighter.objects.weapons.SingleWeapon;
 import engine.graphics.Sprite;
+import engine.sound.SoundEffect;
 import engine.window.Game;
 
 public class BaseEnemyShip extends BaseShip {
+
+    protected boolean immune = false;
 
     public BaseEnemyShip(Game game){
         super(game);
@@ -16,10 +20,38 @@ public class BaseEnemyShip extends BaseShip {
         maxSpeed = 200;
         brakeRatio = 1.6;
 
-        size.set(10 , 28);
+        size.set(38, 28);
+
+        checkForCollisions = true;
 
         currentSprite = new Sprite(64, 48, "/images/Ship_Bombardier.png");
-        currentSprite.origin.set(23,8);
+        currentSprite.origin.set(13,8);
         currentSprite.rotate(180);
+
+        SoundEffect shootSound = new SoundEffect("/sound/sfx/Explosion_01.wav");
+        shootSound.setVolume(.9);
+
+        Sprite explosion = new Sprite(32,32, "/images/Explosion.png");
+        explosion.setFramesPerFrame(5);
+        explosion.origin.set(-4,0);
+
+        onCollision.addListener(gameObject -> {
+
+            if (gameObject instanceof BaseBullet) {
+                BaseBullet bullet = ((BaseBullet) gameObject);
+
+                if (bullet.isExploding()) return;
+
+                bullet.explode();
+
+                if (immune) return;
+
+                shootSound.start();
+                currentSprite = explosion;
+                explosion.onAnimationEnd.addListener(o -> this.destroy());
+
+                immune = true;
+            }
+        });
     }
 }
