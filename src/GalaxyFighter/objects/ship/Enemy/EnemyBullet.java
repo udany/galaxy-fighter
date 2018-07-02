583be9f2232c1d8c33c07dbc4ab51e18406a9c28
udany <long.eagle.goodies@gmail.com>
2018-07-02 03:31:33 -0300

    Green bullet

--- src/GalaxyFighter/objects/ship/Enemy/EnemyBullet.java
+++ src/GalaxyFighter/objects/ship/Enemy/EnemyBullet.java
@@ -14,7 +14,6 @@ public class EnemyBullet extends BaseBullet {
 
         currentSprite = new Sprite(16, 16, "/images/Bullets.png");
         currentSprite.origin.set(5, 5);
-        setType(7);
 
         explodeSprite = new Sprite(16, 16, "/images/Explosion_small.png");
         explodeSprite.origin.set(5, 5);
--- src/GalaxyFighter/objects/ship/Enemy/EnemySingleWeapon.java
+++ src/GalaxyFighter/objects/ship/Enemy/EnemySingleWeapon.java
@@ -11,14 +11,14 @@ public class EnemySingleWeapon extends BaseWeapon {
     public EnemySingleWeapon() {
         cadence = .1;
         shootSound = new SoundEffectPool("/sound/sfx/Shoot_01.wav");
-        shootSound.setVolume(.15);
+        shootSound.setVolume(.05);
     }
 
     public void fire(Stage game, BaseShip ship) {
         EnemyBullet b = new EnemyBullet();
         b.setPosition(ship.getCenter().add(-3, -13));
         b.fire(-1);
-        b.setType(5);
+        b.setType(7);
 
         game.addObject(b);
 
